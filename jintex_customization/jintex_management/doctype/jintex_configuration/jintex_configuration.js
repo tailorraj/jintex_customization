c6e1602a8e1f7d7c597ba8fe70baeb30b94657b6
tailorraj <tailorraj111@gmail.com>
2022-05-31 11:49:04 +0000

    jintex configuration

---
+++ jintex_customization/jintex_management/doctype/jintex_configuration/jintex_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Raaj Tailor and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Jintex Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ jintex_customization/jintex_management/doctype/jintex_configuration/jintex_configuration.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Raaj Tailor and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class JintexConfiguration(Document):
+	pass
